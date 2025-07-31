c0273b6865d1ecdc27b50392fd1530c5b316d0ca
PhatLT <phatlt@unit.com.vn>
2025-07-31 16:31:57 +0700

    refactor chain executor linking and application shutdown handling

--- src/main/java/vn/com/phat/FlowableApplication.java
+++ src/main/java/vn/com/phat/FlowableApplication.java
@@ -7,13 +7,16 @@ import vn.com.phat.chain.ChainExecutor;
 import vn.com.phat.chain.CloseApplication;
 import vn.com.phat.chain.DestroyEngine;
 
+import java.util.Optional;
+
 @Slf4j
 @SpringBootApplication(proxyBeanMethods = false)
 public class FlowableApplication {
 
     public static void main(String[] args) {
-        ChainExecutor.link(new DestroyEngine(), new CloseApplication())
-                .execute(SpringApplication.run(FlowableApplication.class, args), args);
+        var applicationContext = SpringApplication.run(FlowableApplication.class, args);
+        Optional.ofNullable(ChainExecutor.link(new DestroyEngine(), new CloseApplication()))
+                .ifPresent(ex -> ex.execute(applicationContext, args));
         log.info("Application has been closed successfully.");
     }
 }
--- src/main/java/vn/com/phat/chain/ChainExecutor.java
+++ src/main/java/vn/com/phat/chain/ChainExecutor.java
@@ -2,13 +2,23 @@ package vn.com.phat.chain;
 
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class ChainExecutor {
     public abstract void execute(ConfigurableApplicationContext applicationContext, String... args);
     private ChainExecutor next;
 
     public static ChainExecutor link(ChainExecutor first, ChainExecutor... executors) {
         ChainExecutor current = first;
+        if(first == null) return null;
+        if (executors == null)
+            return first;
+
+        Set<ChainExecutor> uniqueExecutors = new HashSet<>();
+        uniqueExecutors.add(first);
         for (ChainExecutor executor : executors) {
+            if(executor == null || !uniqueExecutors.add(executor)) continue;
             current.next = executor;
             current = executor;
         }
